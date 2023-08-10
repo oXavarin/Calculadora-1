4c2fcf2998db72eeca9964c41d409d8336934465
Iago <o.xavarin@gmail.com>
2023-08-10 14:43:42 +0200

    Primer version funcional con las operaciones sencillas (sumar, restar, dividir, multiplicar)

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,88 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Boolean presionado = false;
+        string operation;
+        double num1 = 0;
+        double num2;
+        Boolean igual = false;
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            if (!presionado)
+            {
+                Display.Content = "";
+                presionado = true;
+            }
+            Display.Content += (string)((Button)sender).Content;
+        }
+
+        private void n0_Click(object sender, RoutedEventArgs e)
+        {
+            if (!presionado)
+            { 
+
+                Display.Content = "0";
+            }
+            if (Display.Content != "0")
+            {
+                Display.Content += "0";
+            }
+            else
+            {
+                Display.Content = "0";
+            }
+        }
+
+        private void resultado_Click(object sender, RoutedEventArgs e)
+        {
+            if (!igual)
+            {
+                num2 = Convert.ToDouble(Display.Content);
+
+            }
+            igual = true;
+
+            presionado = false;
+            switch (operation)
+            {
+                case "+":
+                    Display.Content = num1 + num2;
+                    break;
+                case "-":
+                    Display.Content = num1 - num2;
+
+                    break;
+                case "*":
+                    Display.Content = num1 * num2;
+
+                    break;
+                case "/":
+                    Display.Content = num1 / num2;
+                    break;
+            }
+            num1 = Convert.ToDouble(Display.Content);
+        }
+
+        private void borrar_Click(object sender, RoutedEventArgs e)
+        {
+            presionado = false;
+            igual = false;
+            Display.Content = "0";
+        }
+
+        private void operacion(object sender, RoutedEventArgs e)
+        {
+            presionado = false;
+            igual = false;
+            num1 = Convert.ToDouble(Display.Content);
+            operation = (string)((Button)sender).Content;
+            Display.Content = "0";
+        }
+
     }
 }
